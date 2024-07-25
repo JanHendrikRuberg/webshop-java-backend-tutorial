04dda15b6a4833480c7ee1534969cc799ad14772
Jan-Hendrik Ruberg <Jan-Hendrik.Ruberg@t-online.de>
2024-07-25 18:46:24 +0200

    Replace loops by streams/lambdas

--- src/main/java/de/oncoding/webshop/repository/ProductRepository.java
+++ src/main/java/de/oncoding/webshop/repository/ProductRepository.java
@@ -5,51 +5,48 @@ import de.oncoding.webshop.model.ProductResponse;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ProductRepository {
+    List<ProductResponse> products = Arrays.asList(
+            new ProductResponse(
+                    "1",
+                    "AMD Ryzen 9 5950X",
+                    "grsss",
+                    79900,
+                    Arrays.asList("AMD", "Processor")
+            ),
+            new ProductResponse(
+                    "2",
+                    "Intel Core i9-9900KF",
+                    "grsss dfdfdfdf",
+                    37900,
+                    Arrays.asList("Intel", "Processor")
+            ),
+            new ProductResponse(
+                    "3",
+                    "NVIDEA Geforce GTX 1080 Ti Black Edition 11GB",
+                    "grsss",
+                    74900,
+                    Arrays.asList("NVIDEA", "graphics")
+            )
+    );
     public List<ProductResponse> findAll(String tag) {
-        List<ProductResponse> products = Arrays.asList(
-                new ProductResponse(
-                        "1",
-                        "AMD Ryzen 9 5950X",
-                        "grsss",
-                        79900,
-                        Arrays.asList("AMD", "Processor")
-                ),
-                new ProductResponse(
-                        "2",
-                        "Intel Core i9-9900KF",
-                        "grsss dfdfdfdf",
-                        37900,
-                        Arrays.asList("Intel", "Processor")
-                ),
-                new ProductResponse(
-                        "3",
-                        "NVIDEA Geforce GTX 1080 Ti Black Edition 11GB",
-                        "grsss",
-                        74900,
-                        Arrays.asList("NVIDEA", "graphics")
-                )
-        );
         if (tag == null)
             return products;
         else {
-            tag = tag.toLowerCase();
-            List<ProductResponse> filtered = new ArrayList<>();
-            for (ProductResponse p: products) {
-                if (lowercaseTags(p).contains(tag)) {
-                    filtered.add(p);
-                }
-            }
-            return filtered;
+            String lowerCaseTag = tag.toLowerCase();
+
+            return products
+                    .stream()
+                    .filter(p -> lowercaseTags(p).contains(lowerCaseTag))
+                    .collect(Collectors.toList());
         }
     }
 
     private static List<String> lowercaseTags(ProductResponse p) {
-        List<String> lowercaseTags = new ArrayList<>();
-        for (String t: p.getTags()) {
-            lowercaseTags.add(t.toLowerCase());
-        }
-        return lowercaseTags;
+        return p.getTags().stream()
+                .map(tag -> tag.toLowerCase())
+                .collect(Collectors.toList());
     }
 }
