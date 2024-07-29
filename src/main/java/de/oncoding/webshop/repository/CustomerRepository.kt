0733eb169115a7248c8c48030198cadcae63e497
Jan-Hendrik Ruberg <Jan-Hendrik.Ruberg@t-online.de>
2024-07-29 15:10:54 +0200

    Add kotlin plugin. Create Customer model and endpoint

---
+++ src/main/java/de/oncoding/webshop/repository/CustomerRepository.kt
@@ -0,0 +1,20 @@
+package de.oncoding.webshop.repository
+
+import de.oncoding.webshop.model.CustomerResponse
+import java.util.UUID
+
+class CustomerRepository {
+
+    val customers = listOf(
+        CustomerResponse(
+            "1",
+            "Total",
+            "Surprise",
+            "total.surprise@gmail.com"
+        )
+    )
+
+    fun findById(id: String): CustomerResponse? {
+        return customers.find { it.id == id }
+    }
+}
