00d819365e01900b98e1bfecd3eef4cabb3f1a3e
Jan-Hendrik Ruberg <Jan-Hendrik.Ruberg@t-online.de>
2024-08-20 15:16:55 +0200

    Use Caffeine Cache

---
+++ src/main/java/de/oncoding/webshop/config/CacheConfig.kt
@@ -0,0 +1,24 @@
+package de.oncoding.webshop.config
+
+import com.github.benmanes.caffeine.cache.Caffeine
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cache.caffeine.CaffeineCacheManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.util.concurrent.TimeUnit
+
+@EnableCaching
+@Configuration
+class CacheConfig {
+
+    @Bean
+    fun cacheManagerCaffeine(): CacheManager {
+        val cacheManager = CaffeineCacheManager("productsResponses")
+        cacheManager.setCaffeine(
+            Caffeine.newBuilder()
+                .expireAfterAccess(1, TimeUnit.MINUTES)
+        )
+        return cacheManager
+    }
+}
